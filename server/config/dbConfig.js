0b39f9fa20853e9045e006ce51d118b765f7abbc
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-20 18:25:38 +0530

    added

---
+++ server/config/dbConfig.js
@@ -0,0 +1,17 @@
+import mongoose  from "mongoose"; 
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbConnect = () =>{
+    try {
+        mongoose.connect(`mongodb+srv://${process.env.MONGO_CRED}@cluster0.8wdm4gd.mongodb.net/BLOG-API`).then(()=>{
+            console.log('DB Connected :)')
+        }).catch((e)=>{
+            console.log(e)
+        })
+    } catch (error) {
+       console.log(error) 
+    }
+}
+
+export default dbConnect;
