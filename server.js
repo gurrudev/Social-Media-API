0689b2c44d0cc7d36fcc94c40eb578c408619b83
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-22 13:15:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use('/api/users', router)
 app.use('/api/blogs', blog_router)
 
-const PORT = 5000
+const PORT = 3000
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server is running on ${process.env.PORT || PORT}`)
     dbConnect();
