a09d72d1fedeacaa70823d16e6876be594c7a619
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-25 01:25:19 +0530

    updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,7 +4,11 @@ dotenv.config();
 
 const dbConnect = () =>{
     try {
-        mongoose.connect(`mongodb+srv://${process.env.MONGO_CRED}@cluster0.8wdm4gd.mongodb.net/BLOG-API`).then(()=>{
+        mongoose.set('strictQuery', false);
+        mongoose.connect(`mongodb+srv://${process.env.MONGO_CRED}@cluster0.8wdm4gd.mongodb.net/BLOG-API`,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then(()=>{
             console.log('DB Connected :)')
         }).catch((e)=>{
             console.log(e)
