262470fc29e7b6ae1ff2c47150c4475caa5902a0
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2024-01-01 14:16:15 +0530

    schemas updated

--- models/User.js
+++ models/User.js
@@ -5,22 +5,46 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
-    email:{
-        type:String,
-        unique :true,
-        required: true
+    email: {
+        type: String,
+        unique: true,
+        required: true,
     },
     password: {
         type: String,
-        required : true,
+        required: true,
         minlength: 6,
     },
-    blogs:[{type: mongoose.Types.ObjectId, ref:'Blog', required: true}]
-    
-})
+    profile_pic: {
+        type: String,
+        default:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    user_title: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    location: {
+        city: { type: String },
+        country: { type: String },
+    },
+    skills: {
+        type: [String],
+    },
+    blogs: [{ type: mongoose.Types.ObjectId, ref: "Blog", required: true }],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+    },
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
